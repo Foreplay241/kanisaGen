3a9f2e1a45dce705494c6226bb064cbd16c33a7c
Foreplay241 <foreplay241@gmail.com>
2024-01-12 19:51:49 -0800

    taking a step back is a step forward from the future.

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import tkinter as tk
 from idutc import IDUTC
 from texioty import TEXIOTY
-from artay import ARTAY
-from kinvow import KINVOW
 
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.idutc_frame = IDUTC(master=master)
-        self.idutc_frame.grid(column=1, row=0)
+        self.idutc_frame.grid(column=0, row=0)
         self.texioty_frame = TEXIOTY(master=master, IDUTC=self.idutc_frame)
         self.texioty_frame.grid(column=0, row=1)
 
@@ -17,7 +15,7 @@ class Application(tk.Frame):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     root = tk.Tk()
-    root.geometry("1252x800")
+    root.geometry("800x720")
     root.title('KanisaGen')
     app = Application(master=root)
     app.mainloop()
--- texioty.py
+++ texioty.py
@@ -21,6 +21,7 @@ BASE_URL = "https://discord.com/api/v9"
 headers = {
     "Authorization": f"Bot {TOKEN}"
 }
+textbox_width = 88
 
 
 class TEXIOTY(LabelFrame):
@@ -46,10 +47,10 @@ class TEXIOTY(LabelFrame):
         # self.ARTAY_frame: artay.ARTAY = ARTAY
         # self.KINVOW_frame: kinvow.KINVOW = KINVOW
         # self.manny = TalkingPinata()
-        self.texoty = Text(self, height=26, width=69, bg="light blue", relief=SUNKEN)
+        self.texoty = Text(self, height=32, width=textbox_width, bg="light blue", relief=SUNKEN)
         self.texoty.grid(column=0, row=0, rowspan=28)
         self.input_str_var = StringVar()
-        self.texity = Entry(self, width=69, bg="light green", textvariable=self.input_str_var)
+        self.texity = Entry(self, width=textbox_width, bg="light green", textvariable=self.input_str_var)
         self.texity.grid(column=0, row=29)
         # CONFIG LANGUAGE TAGS FOR LATER USE
         self.texoty.tag_config("English", background="light blue", foreground="blue")
@@ -255,10 +256,13 @@ class TEXIOTY(LabelFrame):
                 self.priont_help(self.input_list[1])
             else:
                 self.priont_help()
+
+        # PRINT A LIST OF COMMANDS TO TEXOTY
         if self.input_list[0] == "kommands":
             self.clear_texoty()
             self.priont_commands()
 
+        # PRINT A RANDOMLY GENERATED SENTENCE
         if self.input_list[0] == "RSG":
             self.clear_texoty()
             self.make_random_sentence()
@@ -269,196 +273,19 @@ class TEXIOTY(LabelFrame):
             self.diarySentenceList.append(timestamp_line_entry(st, self.input_list[0],
                                                                lead_line='ts', follow_line=' ' * (
                         self.diary_line_length - len(self.input_list[0]))))
+
         # SET THE LANGUAGE OF TEXOTY
         if self.input_list[0] == "set":
             self.chosen_tongue = self.input_list[1]
             self.texoty.tag_add(self.chosen_tongue, "0.0", END)
             self.configure(text=f'Texioty: {self.chosen_tongue}')
 
-        if self.input_list[0] == "dl":
-            yt = YouTube(self.input_list[1], on_complete_callback=self.priont_string('Completed'))
-            stream = yt.streams.filter(only_audio=True).first()
-            stream.download(output_path="YOUTUBE/")
-            self.priont_string(f"Downloading {yt.title}")
-
         if self.input_list[0] == "disp":
             self.clear_texoty()
             kre8dict = self.IDUTC_frame.kre8dict
             self.priont_dict(kre8dict)
 
-        if self.input_list[0] == "addrecipe":
-            self.ARTAY_frame.add_osrs_recipes()
-
-        if self.input_list[0] == "check":
-            self.clear_texoty()
-            # address = self.IDUTC_frame.kre8dict['use_id']
-            mm_address = '0xb1A955A7Aa511a7EAA8215b8e086ed2C5d5fE916'
-            gs_address = '0xEFc9Fe27d230f89D605d1b59d0BE62fe4da93659'
-            lr_address = '0x7d8e7c819e8a1d5e918d81a7d2d00959e85e3404'
-            my_addresses = [mm_address, gs_address, lr_address]
-            for adrs in my_addresses:
-                loopringAccountDict = self.find_loopring_account(adrs)
-                nftDict = self.check_nfts(loopringAccountDict['accountId'])
-                self.filter_nfts(nftDict, [self.input_list[1]])
-
-        if self.input_list[0] == "kre8":
-            print(self.input_list)
-            self.create_masterpiece()
-
-        if self.input_list[0] == "avatar":
-            self.create_avatar("MOLOKAI", {})
-
-        if self.input_list[0] == "pin":
-            self.pin_masterpiece()
-
-        if self.input_list[0] == "batch":
-            kre8dict = self.IDUTC_frame.kre8dict
-            if not os.path.isdir(kre8dict['name']):
-                os.mkdir(kre8dict['name'])
-            for abt in ["Avatar", "Banner", "Tile"]:
-                self.create_abt(kre8dict, abt)
-            self.create_batch(kre8dict)
-
-        # if self.input_list[0] == "discord":
-        #
-        #     # channel_id_list = [1078081276378087454, 1078370849427562568, 1078371706999148624,
-        #     #                    1078371850754740278, 1078371864684019802, 1078372206029066341]
-        #     channel_id_list = [1078371850754740278]
-        #     for ch_id in channel_id_list:
-        #         messages_path = f'{BASE_URL}/channels/{ch_id}/messages'
-        #         channel_path = f'{BASE_URL}/channels/{ch_id}'
-        #         respo = requests.get(channel_path, headers=headers)
-        #         kre8dict = self.IDUTC_frame.setupKRE8dict(f"r4nd0m", f"4206942069", "Random")
-        #         # kre8dict = self.IDUTC_frame.setupKRE8dict(f"{respo.json()['name']}", f"{ch_id}"[9:], "Random")
-        #         size = set_masterpiece_size(kre8dict['artributes'][3])
-        #         nim = Image.new("RGBA", size, BLUE_VIOLET)
-        #         self.generate_artyles(nim, kre8dict, self.input_list)
-        #         fileName = f"KINVOW/{kre8dict['name']}.png"
-        #         nim.save(fileName)
-        #         files = {
-        #             'file': (f'{fileName}', open(f'{fileName}', 'rb'))
-        #         }
-        #         paylow = {
-        #             "content": fileName
-        #         }
-        #         if "Mujic" in kre8dict:
-        #             mujicFileName = f"MUJIC/{kre8dict['name']}.wav"
-        #             files['file'] = (f'{mujicFileName}', open(f'{mujicFileName}', 'rb'))
-        #             paylow['content'] = mujicFileName
-        #         respo = requests.post(messages_path, json=paylow, headers=headers, files=files)
-        #         print(respo)
         self.input_str_var.set("")
-
-        # if len(text_input) == 1:
-        #     kre8dict = self.IDUTC_frame.kre8dict
-        #     size = set_masterpiece_size(kre8dict['artributes'][3])
-        #     nim = Image.new("RGBA", size, DRS_PURPLE)
-        #     wordie.check_hangman_letter(text_input, kre8dict)
-        #     wordie.update_hangman(nim, kre8dict)
-        #     self.priont_dict(kre8dict)
-        #     save_name = f"Wordie/{kre8dict['artributes'][3]}/{kre8dict['name']}.png"
-        #     nim.save(save_name)
-        #     self.crimg = PhotoImage(file=save_name)
-        #     self.KINVOW_frame.use_canvas.create_image(320, 320, image=self.crimg)
-
-    def upload_discord(self):
-        pass
-
-    def generate_artyles(self, nim: Image, kre8dict: dict, artyle_list: list, abt="Masterpiece"):
-        kre8dict[abt] = {}
-        for artyle in artyle_list:
-            print(f"artyle: {artyle}")
-            if artyle.lower() == "glyth":
-                self.generate_glyth(nim, kre8dict, abt)
-            if artyle.lower() == "glyph":
-                self.generate_glyph(nim, kre8dict, abt)
-            if artyle.lower() == "wordie":
-                self.generate_wordie(nim, kre8dict, abt)
-            if artyle.lower() == "spirite":
-                self.generate_spirite(nim, kre8dict)
-            if artyle.lower() == "foto":
-                self.generate_foto(nim, kre8dict)
-            if artyle.lower() == "recipe":
-                self.generate_recipe(nim, kre8dict)
-            if artyle.lower() == "mujic":
-                self.generate_mujic(nim, kre8dict)
-            if artyle.lower() == "gaym":
-                self.generate_gaym(nim, kre8dict)
-
-    def generate_glyth(self, img: Image, kre8dict: dict, abt="Masterpiece"):
-        """
-        Using the kre8dict, add a glyth to the img.
-        :param img: img to add a glyth to.
-        :param abt: Type of image to generate.
-        :param kre8dict: Instructions on how to add the glyth.
-        :return:
-        """
-        self.KINVOW_frame.generate_glyth(img, kre8dict, abt)
-
-    def generate_glyph(self, img: Image, kre8dict: dict, abt="Masterpiece"):
-        """
-        Using the kre8dict, add a glyph to the img.
-        :param abt: Avatar, Banner or Tile or Masterpiece.
-        :param img: img to add a glyph to.
-        :param kre8dict: Instructions on how to add the glyph.
-        :return:
-        """
-        self.KINVOW_frame.generate_glyph(img, kre8dict, abt)
-
-    def generate_wordie(self, img: Image, kre8dict: dict, abt="Masterpiece"):
-        """
-        Using the kre8dict, add a wordie to the img.
-        :param abt:
-        :param img: img to add a wordie to.
-        :param kre8dict: Instructions on how to add the wordie.
-        :return:
-        """
-        self.KINVOW_frame.generate_wordie(img, kre8dict, abt)
-
-    def generate_spirite(self, img: Image, kre8dict: dict):
-        """
-        Using the kre8dict, add a spirite to the img.
-        :param img: img to add a spirite to.
-        :param kre8dict: Instructions on how to add the spirite.
-        :return:
-        """
-        self.KINVOW_frame.generate_spirite(img, kre8dict)
-
-    def generate_recipe(self, img: Image, kre8dict: dict):
-        """
-        Using the kre8dict, add a recipe to the img.
-        :param img: img to add a recipe to.
-        :param kre8dict: Instructions on how to add the recipe.
-        :return:
-        """
-        self.KINVOW_frame.generate_recipe(img, kre8dict)
-
-    def generate_foto(self, img: Image, kre8dict: dict):
-        """
-        Using the kre8dict, add a foto to the img.
-        :param img: img to add a foto to.
-        :param kre8dict: Instructions on how to add the foto.
-        :return:
-        """
-        self.KINVOW_frame.generate_foto(img, kre8dict)
-
-    def generate_mujic(self, img: Image, kre8dict: dict):
-        """
-        Using the kre8dict, add a mujic to the img.
-        :param img: img to add a mujic to.
-        :param kre8dict: Instructions on how to add the mujic.
-        :return:
-        """
-        self.KINVOW_frame.generate_mujic(img, kre8dict)
-
-    def generate_gaym(self, img: Image, kre8dict: dict):
-        """
-        Using the kre8dict, add a gaym to the img.
-        :param img: img to add a gaym to.
-        :param kre8dict: Instructions on how to add the gaym.
-        :return:
-        """
-        self.KINVOW_frame.generate_gaym(img, kre8dict)
 
     def print_to_texoty(self, string_to_display: str, font_color='blue'):
         self.texoty.configure(fg=font_color)
@@ -549,98 +376,16 @@ class TEXIOTY(LabelFrame):
         self.priont_string(f"-Exiting diary mode-   {end_now}")
         return end_now
 
-    def create_avatar(self, img: Image, kre8dict: dict) -> Image:
-        # size = set_masterpiece_size("Avatar")
-        self.generate_artyles(img, kre8dict, self.input_list, "Avatar")
-
-    def create_banner(self, img: Image, kre8dict: dict) -> Image:
-        # size = set_masterpiece_size("Banner")
-        self.generate_artyles(img, kre8dict, self.input_list, "Banner")
-
-    def create_tile(self, img: Image, kre8dict: dict) -> Image:
-        # size = set_masterpiece_size("Tile")
-        self.generate_artyles(img, kre8dict, self.input_list, "Tile")
-
-    def create_abt(self, kre8dict: dict, abt="Avatar"):
-        """
-        Create an avatar, banner and tile with the given kre8dict.
-
-        :param abt: Which part of the abt?
-        :param kre8dict: Instructions to build avatar/banner/tile.
-        :return:
-        """
-        size = set_masterpiece_size(abt)
-        nim = Image.new("RGBA", size, SUMMER_SKY)
-        if abt == "Avatar":
-            self.create_avatar(nim, kre8dict)
-        if abt == "Banner":
-            self.create_banner(nim, kre8dict)
-        if abt == "Tile":
-            self.create_tile(nim, kre8dict)
-        save_name = f"{kre8dict['name']}/{abt}.png"
-        nim.save(save_name)
-        self.priont_dict(kre8dict)
-
-    def create_masterpiece(self):
-        """
-        Create a masterpiece with the selected artyles from the kommand.
-
-        :return:
-        """
-        kre8dict = self.IDUTC_frame.kre8dict
-        size = set_masterpiece_size(kre8dict["artributes"][3])
-        nim = Image.new("RGBA", size, DRS_PURPLE)
-        if len(self.input_list) > 1:
-            self.generate_artyles(nim, kre8dict, self.input_list)
-            save_name = f"{self.input_list[1].upper()}/{kre8dict['artributes'][3]}/{kre8dict['name']}.png"
-            for c in save_name:
-                if c in "?!&":
-                    save_name = save_name.replace(c, "")
-            nim.save(save_name)
-            self.crimg = PhotoImage(file=save_name)
-            self.KINVOW_frame.use_canvas.create_image(320, 320, image=self.crimg)
-        self.priont_dict(kre8dict)
-
-    def pin_masterpiece(self):
-        kre8dict = self.IDUTC_frame.kre8dict
-        pin_resp = self.manny.pin_masterpiece_pinata(kre8dict["name"], kre8dict["file_path"])
-        kre8dict["image"] += pin_resp["IpfsHash"]
-        kre8dict["animation_url"] += pin_resp["IpfsHash"]
-        self.priont_dict(pin_resp)
-        save_json(kre8dict)
-        self.IDUTC_frame.setUseIDUTC()
-
-    def create_batch(self, kre8dict: dict):
-        batch_size = self.input_list[1]
-        for i in range(int(batch_size)):
-            size = set_masterpiece_size("Dog")
-            nim = Image.new("RGBA", size, DRS_PURPLE)
-            self.generate_artyles(nim, kre8dict, self.input_list)
-            save_name = f"{kre8dict['name']}/{i}_{kre8dict['name']}.png"
-            nim.save(save_name)
-
     def find_loopring_account(self, address: str) -> dict:
         endpoint = self.base_loopring_api_url + "/api/v3/account"
         endpoint += f"?owner={address}"
         response: requests.Response = requests.get(url=endpoint)
         return response.json()
 
-    def check_nfts(self, lr_account_id: int) -> dict:
-        endpoint = self.base_loopring_api_url + f"/api/v3/user/nft/balances"
-        endpoint += f"?accountId={lr_account_id}"
-        lr_headers = {
-            'X-API-KEY': "j0Uvr4vGb4cLZr6TUfnMfW1krU3CaPil10cJPiNCumZCwfUGgZiQQHcVHsvYLGOS"
-        }
-        response: requests.Response = requests.get(url=endpoint, headers=lr_headers)
-        return response.json()
-
     def filter_nfts(self, nft_dict: dict, white_list: list):
         for nft in nft_dict['data']:
             if nft['nftId'] == white_list[0].lower():
                 self.priont_dict(nft)
-
-    def generate_avatar(self, nim, kre8dict):
-        pass
 
     def priont_commands(self):
         self.priont_dict(self.commands_dict)
